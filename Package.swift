14d738553d7b47cad568c0ed0835ee119ce457f3
RUBEN LOPEZ <ruben.lopez@tiendeo.com>
2020-09-07 17:25:43 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -30,12 +30,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GovernorSwiftPackage",
-            dependencies: []),
-        .testTarget(
-            name: "GovernorSwiftPackageTests",
-            dependencies: ["GovernorSwiftPackage"]),
         .binaryTarget(name: "Governor",
                       url:"https://tiendeo.jfrog.io/artifactory/pods/Governor/Governor-1.0.5-APPS-1400-beta24.zip", checksum: "7f42fd9ad42939d65048fe535f9dd13078e4b35c59e9d8775a3f8b8eb9ec40ea")
     ]
