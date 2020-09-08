f57f8b934b944f43a7cb293ff462672c5f2f9c63
RUBEN LOPEZ <ruben.lopez@tiendeo.com>
2020-09-08 13:24:54 +0200

    fix binary target

---
+++ Governor.xcframework/ios-i386_x86_64-simulator/Governor.framework/Headers/Governor.h
@@ -0,0 +1,19 @@
+//
+//  Governor.h
+//  Governor
+//
+//  Created by Juan Luis Montero Roca on 3/10/17.
+//  Copyright © 2017 Tiendeo Web Marketing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Governor.
+FOUNDATION_EXPORT double GovernorVersionNumber;
+
+//! Project version string for Governor.
+FOUNDATION_EXPORT const unsigned char GovernorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Governor/PublicHeader.h>
+
+
